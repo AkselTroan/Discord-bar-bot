6fc10232e7aa0f04587fedbaac581aa55ed0b71e
akseltroan <aksel_troan_98@hotmail.com>
2021-03-17 20:31:02 +0100

    Added muting function. The ambience will now loop.

--- barBot.py
+++ barBot.py
@@ -2,10 +2,8 @@ import discord
 import asyncio
 from discord.ext import commands
 from discord.utils import get
-from youtube_dl import *
 import time
 import random
-
 import os
 
 
@@ -30,9 +28,10 @@ rmenu.close()
 
 # Currently valid drinks
 openDrinks = ["Tuborg", "IPA", "Stout", "Wine", "Cider", "Shot"] # Maybe add a 'Ask bartender to order a new drink to the bar' function?
+
 members = []
 isSeated = False
-
+muting = True # Default setting for "Too drunk" muting function
 
 
 class Member:  # Class for each member in the voice channel. Keeping track of sit state and drink count
@@ -41,6 +40,7 @@ class Member:  # Class for each member in the voice channel. Keeping track of si
         self.name = name
         self.isSeated = False
         self.drink = []
+        self.hasWalked = False
 
     def add_drink(self, drink):
         self.drink.append(drink)
@@ -52,6 +52,7 @@ class Member:  # Class for each member in the voice channel. Keeping track of si
         self.isSeated = True
 
 
+
 @client.event
 async def on_ready():
     print('Bot online.') # Gives the host confirmation that the bot is running correctly
@@ -59,11 +60,17 @@ async def on_ready():
 
 @client.command(pass_context=True)  # Bot join the voice channel
 async def start(ctx):
-
+    
+    global muting, isSeated, members  # Resets settings and members
+    muting = True
+    isSeated = False
+    members = []
+    
     channel = ctx.message.author.voice.channel
     vc = await channel.connect()
 
-    vc.play(discord.FFmpegPCMAudio(executable="PATHWAY TO YOUR ffmpeg.exe", source='PATHWAY TO YOUR bar_ambiance.mp3'))
+    # The ffmpeg pathway is for linux. Different pathway if you are on Windows
+    vc.play(discord.FFmpegPCMAudio(executable="/bin/ffmpeg", source='PATHWAY TO YOUR MP3 FILE', before_options="-stream_loop -1"))
 
     await ctx.message.channel.send(welcome)  # Outputs welcome text to current text channel
 
@@ -71,6 +78,21 @@ async def start(ctx):
 @client.command()  # Bot leaving the voice channel
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+    running = False
+
+
+
+@client.command()
+async def mute(ctx): # Change mute setting
+    global muting
+
+    if muting is False:
+        await ctx.channel.send("```diff\n+ Changing muting setting to True\n```")
+        muting = True
+    
+    else:
+        await ctx.channel.send("```diff\n- Changing muting setting to False\n```")
+        muting = False
 
 
 @client.command()
@@ -127,18 +149,42 @@ async def barhelp(message):
 
 
 @client.command()
-async def buy(message, drink):
-    author = str(message.author).split("#", 1)
+async def buy(ctx, drink):
+    author = str(ctx.author).split("#", 1)
     member = Member(author[0])
     for obj in members:
         if obj.name == member.name:
             member = obj
 
-    if drink in openDrinks: # Validate users input. 1 keep track of drinks. 2 Keep the users from inputting inappropriate submissions
-        await message.channel.send(f"{author[0]} just bought a {drink}! Enjoy my dude")
-        member.add_drink(drink)
+    global muting
+    if member.drink_count() >= 7 and muting is True:
+
+        if member.hasWalked:
+            if drink in openDrinks:
+                await ctx.channel.send(f"This is the last one {author[0]}!, here is your {drink}")
+                member.add_drink(drink)
+                member.hasWalked = False
+
+        else:
+
+            await ctx.channel.send("That is enough! Take a walk. Come back in 1 minute")
+            
+            # Member getting muted
+            await discord.Member.edit(ctx.author, mute=True, reason="Too drunk")
+            await asyncio.sleep(60)
+            member.hasWalked = True
+
+            # Member gets unmuted after 60 seconds
+            await discord.Member.edit(ctx.author, mute=False, reason="Has walked")
+            await ctx.channel.send(f"Welcome back {member.name}!")
+
     else:
-        await message.channel.send(f"I'm sorry {author[0]}, I don't think we have that drink in the bar")
+
+        if drink in openDrinks: # Validate users input. 1 keep track of drinks. 2 Keep the users from inputting inappropriate submissions
+            await ctx.channel.send(f"{author[0]} just bought a {drink}! Enjoy my dude")
+            member.add_drink(drink)
+        else:
+            await ctx.channel.send(f"I'm sorry {author[0]}, I don't think we have that drink in the bar")
 
 
 @client.command()
@@ -148,9 +194,21 @@ async def amidrunk(message):
     for obj in members:
         if obj.name == member.name:
             member = obj
+    
     await message.channel.send(f"You've had {member.drink_count()} drink(s). These were: {', '.join(member.drink)}")
-    if member.drink_count() <= 2:
+    
+    if member.drink_count() == 2 or member.drink_count() == 3:
         await message.channel.send("You are not to drunk you can stay a bit more")
+    
+    elif member.drink_count() == 4 or member.drink_count() == 5:
+        await message.channel.send("You're beginning look drunk. Slow down")
+    
+    elif member.drink_count() == member.drink_count() == 6:
+        await message.channel.send("You are drunk. Do not drink more")
+
+    elif member.drink_count() > 7 and muting is True:
+        await message.channel.send("You seem a bit better after your walk.")
+
 
 
 @client.command()
@@ -191,12 +249,5 @@ async def never(ctx):  # Never have I ever
     await ctx.channel.send(statement)
 
 
-# ====================== WORK IN PROGRESS =========================== #
-#@client.command(pass_context=True)
-#async def busride(ctx):
-    # Currently working on implementing busride drinking. 
-    # The bot will send your pair of cards in private message and the bot will steer the flow of the game and manage the card of the table (these card will be printed in the chat)
-#    await ctx.author.send('Ready for busride drinking game. Here are your cards:') # Sends private message to the author
-# ====================== WORK IN PROGRESS  ========================== #
-client.run('Insert Your Token Here!')
+client.run("INSERT YOUR TOKEN HERE") # Secret token!
 
